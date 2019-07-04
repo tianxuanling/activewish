47780a72944f9ed0d322642c8a18613952243af2
tianxuanling <623911519@qq.com>
2019-07-04 14:34:02 +0800

    Producer modify

--- src/main/java/com/txl/activewish/ActiveWish/kafka/KafkaProducerTest.java
+++ src/main/java/com/txl/activewish/ActiveWish/kafka/KafkaProducerTest.java
@@ -11,7 +11,7 @@ public class KafkaProducerTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Properties props = new Properties();
-		props.put("bootstrap.servers", "192.168.56.40:9092");
+		props.put("bootstrap.servers", "192.168.100.40:9092");
 		props.put("acks", "all");
 		props.put("retries", 0);
 		props.put("batch.size", 16384);
@@ -21,8 +21,8 @@ public class KafkaProducerTest {
 		props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
 		Producer<String, String> producer = new KafkaProducer<>(props);
-		for (int i = 0; i < 1000000; i++) {
-			producer.send(new ProducerRecord<String, String>("kafka-test1", "test" + i, "test" + i));
+		for (int i = 0; i < 1000; i++) {
+			producer.send(new ProducerRecord<String, String>("kafka-test1", "test" + i, "{"+ i +"/mnt/activewishfiles/包装资料共享平台/发货唛头和标牌/1908-999矿山建材产品/0708-1708/0808-999/0808-042D J493M新标牌（121+3福建）第2台.doc}"));
 			System.out.println("kafka-test1" + i);
 		}
 
